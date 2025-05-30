67ce82f482b36264f668be903d94d466e6d4a1ca
Josué Rogério <josue.otakusensei@gmail.com>
2025-05-30 09:27:36 -0300

    Criando a seção "PROJETOS" e o componente "PROJETO"

---
+++ src/components/Projeto/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const Card = styled.div`
+    border: 1px solid #C1C1C1;
+    padding: 16px;
+`
+
+export const LinkBotao = styled.button`
+    color: #fff;
+    font-size: 14px;
+    background-color: #4476bf;
+    text-decoration: none;
+    padding: 8px;
+    display: inline-block;
+    border: none;
+    margin-top: 24px;
+`
