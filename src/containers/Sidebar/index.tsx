7b59f1dfaceb5194022f6ddc6749acc3f9909638
Josué Rogério <josue.otakusensei@gmail.com>
2025-05-30 10:28:12 -0300

    Estilizando a "Sidebar"

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,17 @@
 import Titulo from '../../components/Titulo'
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+import { BotaoTema, Descricao, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/josue-rgt.png" alt="" />
-    <Titulo fontSize={20}>Josué Rogério</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Josué Rogério</Titulo>
+      <Paragrafo tipo='secundario' fontSize={16}>josue-rgt</Paragrafo>
+      <Descricao tipo='principal' fontSize={12}>Engenheiro front-end</Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
